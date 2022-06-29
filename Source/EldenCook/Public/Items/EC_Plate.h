59035d732af92f154559fb5dbe46c05165880df7
pedroh154 <pedrohbr154@gmail.com>
2022-06-29 20:09:17 -0300

    created plate class

---
+++ Source/EldenCook/Public/Items/EC_Plate.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/EC_Item.h"
+#include "EC_Plate.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELDENCOOK_API AEC_Plate : public AEC_Item
+{
+	GENERATED_BODY()
+	
+};
